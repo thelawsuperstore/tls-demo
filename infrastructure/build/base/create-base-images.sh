a2807b59f963e895c16f6bb6c623e082246f3710
Kevin Swinton <kevin.swinton@reallymoving.com>
2024-12-20 12:16:16 +0000

    Testing

--- infrastructure/build/base/create-base-images.sh
+++ infrastructure/build/base/create-base-images.sh
@@ -11,16 +11,16 @@ dockerHubAct="reallymovinggroup"
 dockerHubRpo="tls-demo"
 imgDatestamp=$(date '+%Y%m%d')
 
-# build and push the base container image for "web"
+# build and push the base container image for "web" (don't build for linux/amd64 in K3S)
 dockerHubImg="web-base"
 dockerHubImg=$dockerHubAct/$dockerHubRpo:$dockerHubImg-$imgDatestamp
 echo "[INFO] : Will build and push : $dockerHubImg"
 docker buildx build . -f Dockerfile.web --no-cache --platform linux/amd64,linux/arm64 --tag $dockerHubImg
 docker push                                       $dockerHubImg
 
-# build and push the base container image for "app"
+# build and push the base container image for "app" (don't build for linux/amd64 in K3S)
 dockerHubImg="app-base"
 dockerHubImg=$dockerHubAct/$dockerHubRpo:$dockerHubImg-$imgDatestamp
 echo "[INFO] : Will build and push : $dockerHubImg"
-docker buildx build . -f Dockerfile.app --no-cache --platform linux/amd64,linux/arm64 --tag $dockerHubImg
+docker buildx build . -f Dockerfile.app --no-cache --platform linux/arm64 --tag $dockerHubImg
 docker push                                       $dockerHubImg
--- infrastructure/build/final/Dockerfile.app
+++ infrastructure/build/final/Dockerfile.app
@@ -1,4 +1,4 @@
-FROM reallymovinggroup/tls-demo:app-base-20241219
+FROM reallymovinggroup/tls-demo:app-base-20241220
 
 # Set the working directory
 WORKDIR /var/www/html
