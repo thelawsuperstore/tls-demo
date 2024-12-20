33846f4ec055fcb3c874c15b8f067182589bffc4
Kevin Swinton <kevin.swinton@reallymoving.com>
2024-12-20 15:03:21 +0000

    Testing

--- infrastructure/build/final/code/source/index.php
+++ infrastructure/build/final/code/source/index.php
@@ -3,4 +3,4 @@
     // echo something useful for now
     print "PHP has just run at : " . date('Y-m-d H:i:s');
 
-    print "aha - built from the 'main' branch don't forget!";
+    print "update to time things";
